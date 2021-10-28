7b433385e0e9cd98c96182c25ce3c47b4168c792
Matthew Laude <laude_matthew@yahoo.com>
2021-10-28 08:55:56 -0400

    Project2: index and show pages operational

--- server.js
+++ server.js
@@ -53,12 +53,71 @@ app.use(express.static("public"))
 // ROUTES
 
 app.get("/", (req, res) => {
-    res.send("this thing on?")
+    res.send("we good?")
 })
 
 // Seed route
+app.get("/smash/seed", (req, res) => {
+    const seed =
+    [
+      {
+        name: 'Waluigi',
+        img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages-wixmp-ed30a86b8c4ca887773594c2.wixmp.com%2Ff%2F25247b92-6844-4fef-8ed8-5055cc35bf58%2Fdbuztua-01c11142-f1bc-4322-a9b6-28de4044364a.png%2Fv1%2Ffill%2Fw_1001%2Ch_798%2Cstrp%2Fwaluigi_promo_art_recreation_render_by_nintega_dario_dbuztua-pre.png%3Ftoken%3DeyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTA4MCIsInBhdGgiOiJcL2ZcLzI1MjQ3YjkyLTY4NDQtNGZlZi04ZWQ4LTUwNTVjYzM1YmY1OFwvZGJ1enR1YS0wMWMxMTE0Mi1mMWJjLTQzMjItYTliNi0yOGRlNDA0NDM2NGEucG5nIiwid2lkdGgiOiI8PTEzNTQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.kJuxH-4aIoTPgWsJ0qNqJSkIIfvN1v7Svz1HuFX2wQ8&f=1&nofb=1',
+        description: "He plays the role of Luigi's arch-rival and accompanies Wario in spin-offs from the main Mario series, often for the sake of causing mischief and problems.",
+        b: 'Liar ball',
+        sideb: 'Whirluigi',
+        upb: 'Twist dunk',
+        downb: 'Drop rocket',
+        fsimg: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FTY2XcCH3ags%2Fmaxresdefault.jpg&f=1&nofb=1',
+        fs: 'Bob-ombs away'
+      }, {
+        name: 'Knuckles',
+        img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2Fe9e_Zq0W6iI%2Fmaxresdefault.jpg&f=1&nofb=1',
+        description: 'Ugandan knuckles',
+        b: 'drill claw',
+        sideb: 'hammer punch',
+        upb: 'spiral upper',
+        downb: 'spin dash',
+        fsimg:'',
+        fs: 'maximum heat knuckles attack'
+      }
+    ]
+    // delete all 
+    Smash.deleteMany({})
+    .then((data) => {
+        // seed the starter data
+        Smash.create(seed)
+        .then((data) => {
+            // send created data back as JSON
+            res.json(data)
+        })
+    })
+})
 
 
+
+// Index route 
+app.get("/smash", (req, res) => {
+    Smash.find({})
+    .then((fighters) => {
+        res.render("index.liquid", {fighters})
+    })
+    .catch((error) => {
+        res.json({error})
+    })
+})
+
+// Show route ❌
+app.get("/smash/:id", (req, res) => {
+    const id = req.params.id
+    Smash.findById(id)
+    .then((fighter) => {
+        res.render("show.liquid", {fighter})
+    })
+    .catch((error) => {
+        res.json({error})
+    })
+})
 
 ////////////////////////////////////////
 
